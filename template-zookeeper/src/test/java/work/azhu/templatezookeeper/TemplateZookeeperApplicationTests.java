187678ff86c27e07b66502b4aae7759c2537edf1
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-10-21 16:25:29 +0800

    :art:创建一个新的zookeeper注册中心模块

---
+++ template-zookeeper/src/test/java/work/azhu/templatezookeeper/TemplateZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package work.azhu.templatezookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
